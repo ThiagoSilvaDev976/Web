b5ddd971e4185cf5e21d49a35542ee5b86c2a7d1
ThiagoSilvaDev976 <thiago.programador.976@gmail.com>
2024-11-11 09:13:06 -0300

    Teste

--- Aula16.php
+++
@@ -1,9 +0,0 @@
-<?php //comenta-se uma linha com barra dupla, varias com /*
-/*echo "Olá, mundo! <br>"; //echo é o comando de escrita
-$nome = "Thiago"; //declaramos variáveis usando um $
-echo $nome;
-echo "<br> Bem vindo, $nome"; //variavel fica dentro das aspas*/
-$usuario = $_POST["usuario"]; //usuario vem do name do input
-$senha   = $_POST["senha"];
-echo "Bem_vindo $usuario sua senha é $senha";
-?>
